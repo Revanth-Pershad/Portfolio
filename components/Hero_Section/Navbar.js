1b5c01d5849d45a2c2fc400e37269750e82e26f4
Puli Revanth Pershad <revanthpershad@gmail.com>
2023-03-13 18:08:48 +0530

    Changed title

--- components/Hero_Section/Navbar.js
+++ components/Hero_Section/Navbar.js
@@ -3,13 +3,14 @@ const Navbar = () => {
     <nav className="text-white p-6" >
       <div className="flex justify-between">
         <div className="p-2 text-2xl">
-          <div className="font-serif" >Gdsc Cvr Submission</div>
+          <div className="font-serif" >Portfolio</div>
         </div>
         <div className="p-2 text-md">
           <span className="mr-8 hidden md:inline cursor-pointer">Home</span>
           <span className="mr-8 hidden md:inline cursor-pointer">About Me</span>
           <span className="mr-8 hidden md:inline cursor-pointer">Skills</span>
           <span className="mr-8 hidden md:inline cursor-pointer">Contact Me</span>
+          <span className="mr-8 hidden md:inline cursor-pointer">Socials</span>
         </div>
       </div>
     </nav >
